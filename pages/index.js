6ff277ea8fc2b1fbebbb717aec04e8a5e224f3f6
Gustav Hansen <kusti8@gmail.com>
2019-02-25 19:13:27 +0000

    Setup framework

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+class Index extends React.Component {
+    render() {
+        return (
+            <p>Welcome to our band!</p>
+        )
+    }
+}
+
+export default Index
